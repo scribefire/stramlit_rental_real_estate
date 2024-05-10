88f21ef6c4fc926d7b5b3bbeb643e69a04039a02
scribefire <37069861+scribefire@users.noreply.github.com>
2024-05-10 19:37:03 +0900

    Add files via upload

--- streamlit_predict.py
+++ streamlit_predict.py
@@ -6,10 +6,21 @@ model = load("model_ridge.pkl")
 
 
 def user_input_features():
-    house_area = st.sidebar.slider("面積(m2)", 0.0, 200.0, 30.0)
-    distance = st.sidebar.slider("駅からの距離(m)", 1, 2000, 160)
+    house_area = st.sidebar.slider("面積(m2)", 0.0, 250.0, 30.0)
+    year_from_built = st.sidebar.slider("築年数(年)", 0.0, 60, 10.0)
+    distance = st.sidebar.slider("駅からの距離(m)", 1, 7000, 160)
+    balcony_area = st.sidebar.slider("バルコニーの面積(m2)", 0.0, 40.0, 30.0)
+    floor = st.sidebar.slider("部屋の階数(階)", 1, 37, 3)
+    total_floor = st.sidebar.slider("建物の総階数(階)", 1, 47, 3)
 
-    data = {"house_area": [house_area], "distance": [distance]}
+    data = {
+        "house_area": [house_area],
+        "year_from_built": [year_from_built],
+        "distance": [distance],
+        "balcony_area": [balcony_area],
+        "floor": [floor],
+        "total_floor": [total_floor],
+    }
 
     features = pd.DataFrame(data)
     return features
